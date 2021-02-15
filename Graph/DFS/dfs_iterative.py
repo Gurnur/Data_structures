beb410a5150b7fb36b82503293f71850ab645313
Wadhwani, Gurnur | Gur | PPD <gurnur.wadhwani@rakuten.com>
2021-02-16 06:27:38 +0900

    Add dfs

---
+++ Graph/DFS/dfs_iterative.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+class Graph:
+    """
+    Approach: Iterative DFS using stack
+    TC: O(V+E)
+    SC: O(V)
+    """
+    def __init__(self, n, edges):
+        self.n = n
+        self.graph = defaultdict(list)
+        # use set for visited for O(1) lookup
+        # or use visited = [False for i in range(self.n)]
+        self.visited = set()
+        self.res = []
+
+        # add edges for undirected graph
+        for (src, dst) in edges:
+            self.graph[src].append(dst)
+            self.graph[dst].append(src)
+
+    def dfs(self, node):
+        stack = [node]
+        self.visited.add(node)
+
+        while stack:
+            node = stack.pop()
+            self.res.append(node)
+            for adj in self.graph[node]:
+                if adj not in self.visited:
+                    stack.append(adj)
+                    self.visited.add(adj)
+
+    # call DFS on every node for disconnected graph
+    def dfs_iterative(self):
+        for i in range(self.n):
+            if i not in self.visited:
+                self.dfs(i)
+        return self.res
+n = 6
+edges = [(1, 0), (3, 4), (0, 2), (5, 5), (1, 2)]
+obj = Graph(n, edges)
+print(obj.dfs_iterative())
+
+
+
+
+    
--- Graph/DFS/dfs_recursive.py
+++ Graph/DFS/dfs_recursive.py
@@ -1,19 +1,25 @@
 from collections import defaultdict
 
 class Graph:
+    """
+    Approach: Recursive DFS
+    TC: O(V+E)
+    SC: O(V)
+    """
     def __init__(self):
         self.graph = defaultdict(list)
+        self. res = []
     
     def add_edge(self, u, v):
         self.graph[u].append(v)
 
     def dfs_recur(self, source, visited):
         visited[source] = True
-        print(source)
+        self.res.append(source)
 
-        for i in self.graph[source]:
-            if visited[i] == False:
-                self.dfs_recur(i, visited)
+        for adj in self.graph[source]:
+            if visited[adj] == False:
+                self.dfs_recur(adj, visited)
 
     def dfs(self):
         v = len(self.graph) + 1
@@ -22,6 +28,8 @@ class Graph:
         for i in range(v):
             if visited[i] == False:
                 self.dfs_recur(i, visited)
+        
+        return self.res
 
 g = Graph()
 g.add_edge(0, 1) 
@@ -33,4 +41,4 @@ g.add_edge(3, 3)
 g.add_edge(4, 5)
 g.add_edge(4, 3)
 
-g.dfs()
+print(g.dfs())
